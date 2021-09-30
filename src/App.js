e35e518c9155cb1750e1a20c9225ab312a8f40fb
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-30 17:57:02 +0200

    add buttons

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import './App.css';
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import CharacterGallery from "./components/CharacterGallery";
-
 import RickAndMorty from './components/characters.json';
+import {useState} from "react";
 
 function App() {
 
-    const characters = RickAndMorty.results
+    const [characters, setCharacters] = useState([]);
+
+    const showCards = (() => {
+        setCharacters (RickAndMorty.results);
+    })
+
+    const clearCards = (() => {
+        setCharacters ([]);
+    })
+
+    const searchName = ((event) => {
+        const searchParam = event.target.value
+        setCharacters (RickAndMorty.results.filter(
+            (character) => character.name.toLowerCase().includes(searchParam.toLowerCase())
+        ))
+    })
 
     return (
         <>
             <div>
                 <Header title="my fancy otter gallery"/>
                 <Navigation/>
+                <div className="buttonInput">
+                    <button className="buttonSet btn btn-secondary" onClick={showCards} >Show cards</button>
+                    <input className="searchCharacterName" onChange={searchName} />
+                    <button className="buttonClear btn btn-secondary" onClick={clearCards} >Clear!</button>
+                </div>
                 <CharacterGallery characters={characters}/>
             </div>
         </>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,15 @@
 import './Navigation.css'
 
-export default function Navigation(){
+export default function Navigation() {
 
-    return(
-        <navigation className="navigation">
-            <div className="navigation__home">home</div>
-            <div className="navigation__article">article</div>
-            <div className="navigation__random">random</div>
-            <div className="navigation__gallery">gallery</div>
+    return (
+        <navigation className="navigation container">
+            <div className="row justify-content-md-center">
+                <div className="navigation__home col-2">home</div>
+                <div className="navigation__article col-2">article</div>
+                <div className="navigation__random col-2">random</div>
+                <div className="navigation__gallery col-2">gallery</div>
+            </div>
         </navigation>
     )
 }
